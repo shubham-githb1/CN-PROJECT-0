cbbca96e2ae130b6d0fb2213ee4da0d694bc3251
shubham-githb1 <85453513+shubham-githb1@users.noreply.github.com>
2021-11-18 22:51:56 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,24 +1,62 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 
+const myPeer = new Peer({
+  host:'peerjs-server.herokuapp.com', 
+  secure:true, port:443}
+  )
 
-let myVedioStream;
-const videoGrid = document.getElementById("video-grid");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
-
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
 navigator.mediaDevices.getUserMedia({
-    video:true,
-    audio:true
+  video: true,
+  // audio: true
 }).then(stream => {
-    myVedioStream = stream;
-    addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    //connectToNewUser(userId, stream)
+    setTimeout(() => {
+      // user joined
+      connectToNewUser(userId, stream)
+    }, 1000)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
 }
-)
 
-
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream
+function addVideoStream(video, stream) {
+  video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
   videoGrid.append(video)
-} 
+}
